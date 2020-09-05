f2de41d00aadb4019c81e0b37f14528f4d205b4e
LandingHeart <land.liu0123@gmail.com>
2020-09-05 16:55:02 -0400

    boss cirle projectile

--- Assets/LaserScript.cs
+++ Assets/LaserScript.cs
@@ -6,22 +6,24 @@ public class LaserScript : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D rb;
-    public PlayerScript playerTransform;
-    private Vector2 moveDir;
-    public float bulletSpeed = 20f;
+   
+    private Vector3 moveDir;
+    public float bulletSpeed = 30f;
+    private GameObject player;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        playerTransform = GameObject.FindObjectOfType<PlayerScript>();
+        //rb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
 
-        moveDir = (playerTransform.transform.position - transform.position).normalized * bulletSpeed;
-        rb.velocity = new Vector2(moveDir.x, moveDir.y);
-        
+        //moveDir = (playerTransform.transform.position - transform.position).normalized * bulletSpeed;
+        //rb.velocity = new Vector2(moveDir.x, moveDir.y);
+        moveDir = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        //transform.rotation = Quaternion.LookRotation(moveDir);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, moveDir, bulletSpeed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/CircleBullet.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    int numProjectiles;
+
+    [SerializeField]
+    GameObject myProjectile;
+
+    Vector2 starpos;
+
+    private float radius, speed;
+    public static bool fire = false;
+    private bool toFire = false;
+
+    private float fireRate = 1f;
+    private float nextFire;
+
+    public GameObject launchPoint;
+
+    void Start()
+    {
+        radius = 5f;
+        speed = 20f;
+        spawnProjectile(numProjectiles);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+      
+            //starpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                spawnProjectile(numProjectiles);
+            }
+        
+    }
+    void spawnProjectile(int num)
+    {
+        float angleStep = 360f / num;
+        float angle = 0f;
+
+        for (int i = 0; i <= num - 1; i++)
+        {
+            float dirXpos = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+
+            float dirYpos = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+
+            Vector3 projectileVector = new Vector3(dirXpos, dirYpos, 0);
+            Vector3 projectileMoveDir = (projectileVector - launchPoint.transform.position).normalized * speed;
+
+            var proj = Instantiate(myProjectile, launchPoint.transform.position, Quaternion.identity);
+            proj.GetComponent<Rigidbody2D>().velocity =
+                new Vector2(projectileMoveDir.x, projectileMoveDir.y);
+
+            angle += angleStep;
+            Destroy(proj, 1f);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
