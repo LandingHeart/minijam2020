e3891f6c18ff8fbb163af99fc6440531869483bb
LandingHeart <land.liu0123@gmail.com>
2020-09-06 15:18:12 -0400

    flamethrower

---
+++ Assets/AimAtMouse.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimAtMouse : MonoBehaviour
+{
+    private float fireRate = 0.1f;
+    private float nextFire;
+
+    public GameObject flameThrowerPoint; 
+
+    void Start()
+    {
+        flameThrowerPoint.SetActive(false);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (ParticleScript.on)
+        {
+            flameThrowerPoint.SetActive(true);
+        }
+        if(ParticleScript.on == false) {
+            flameThrowerPoint.SetActive(false);
+        }
+
+       
+
+        Vector3 mousepos = Input.mousePosition;
+        mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+
+        Vector2 dir = new Vector2(mousepos.x - transform.position.x, mousepos.y - transform.position.y);
+
+        transform.up = dir;
+       
+    }
+ 
+}
---
+++ Assets/ParticleScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    ParticleSystem ps;
+    float time = 5;
+    public static bool on = false;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ps.playOnAwake = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+        if (Input.GetKeyDown(KeyCode.F)){
+            ps.Play();
+            on = true;
+        }
+
+
+        if (on)
+        {
+            time -= Time.time;
+            if (time < 10f)
+            {
+                ps.Stop();
+                on = false;
+                time = 5;
+            }
+        }
+
+    }
+   
+    private void OnParticleTrigger()
+    {
+        
+    }
+}
