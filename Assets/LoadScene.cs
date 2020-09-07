6628e95b4912de1f35bdc68e4bf6963f4d9e2098
LandingHeart <land.liu0123@gmail.com>
2020-09-06 20:16:36 -0400

    restart button

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject restartButton;
+    public GameObject gameOverText;
+    //reload scene
+    private void Start()
+    {
+        //restartButton.SetActive(true);
+        //gameOverText.SetActive(true);
+    }
+    public void LoadMainScene() {
+        SceneManager.LoadScene("Main");
+    }
+    private void Update()
+    {
+        if (player == null)
+        {
+
+            restartButton.SetActive(true);
+            gameOverText.SetActive(true);
+
+        }
+    }
+
+
+}
