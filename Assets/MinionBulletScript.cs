1f930f588b7d066a9080c8471d2e9bd7fef940a8
wwkb123 <wwkb1233@gmail.com>
2020-09-06 22:38:17 -0400

    update minions

---
+++ Assets/MinionBulletScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionBulletScript : MonoBehaviour
+{   
+    [SerializeField] PlayerScript.myColors bulletColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {  
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Debug.Log("Hit player " + bulletColor);
+            PlayerScript playerScript = collision.gameObject.GetComponent<PlayerScript>();
+            playerScript.TakeDamage(5f, bulletColor);
+            Destroy(gameObject);
+        }
+    }
+}
+
